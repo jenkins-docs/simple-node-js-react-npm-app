3300edd0985bdb2f93ed93db6a55f2ff0ed0e25d
Giles Gaskell <gilesgas@gmail.com>
2017-10-14 23:00:59 +1100

    Fix typo.

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -7,7 +7,7 @@ echo 'https://docs.npmjs.com/cli/install, the "--save-dev" flag causes the'
 echo '"cross-env" dependency to be installed as "devDependencies". For the'
 echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
-echo 'flag. For a comprehensive exaplanation about "devDependencies", see'
+echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
 # npm install --save-dev cross-env
