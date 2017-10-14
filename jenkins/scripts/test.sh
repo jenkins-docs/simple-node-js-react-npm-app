48bbae645e42f8c9e922833a4ac43f34ecb2df7d
Giles Gaskell <gilesgas@gmail.com>
2017-10-14 11:09:24 +1100

    Improve description.

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -5,9 +5,9 @@ echo 'dependency into the local "node_modules" directory, which will ultimately'
 echo 'be stored in the Jenkins home directory. As described in'
 echo 'https://docs.npmjs.com/cli/install, the "--save-dev" flag causes the'
 echo '"cross-env" dependency to be installed as "devDependencies". For the'
-echo 'purposes of this tutorial, this is not important. However, this is'
-echo 'typically how this dependency would be installed. For a comprehensive'
-echo 'exaplanation about "devDependencies", see'
+echo 'purposes of this tutorial, this flag is not important. However, when'
+echo 'installing this dependency, it would typically be done so using this'
+echo 'flag. For a comprehensive exaplanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
 # npm install --save-dev cross-env
