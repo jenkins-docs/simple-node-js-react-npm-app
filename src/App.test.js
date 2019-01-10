4459c1aa3369572f9670b35682b7c3faf9a5dbc2
Cesar Ivan Buenrostro Segura <c35xr@Cesars-MacBook-Pro.local>
2019-01-10 05:33:59 -0600

    test file modified

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('li');
+  const div = document.createElement('table');
   ReactDOM.render(<App />, div);
 });
