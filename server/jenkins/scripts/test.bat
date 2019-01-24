6df9f8d92f288cac1514144abe5a1d8bd8f4845f
Tharit Wichiankoo <tharit_8@hotmail.com>
2019-01-24 20:57:03 +0700

    fix bug test server

--- server/jenkins/scripts/test.bat
+++ server/jenkins/scripts/test.bat
@@ -8,7 +8,7 @@ echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
-cd %cd%/client
+cd %cd%/server
 set -x
 set +x
 
@@ -16,4 +16,3 @@ echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
-npm test
