bcf1e09c7607ab5f5abd83bd27b7ef455844cfa6
Tharit Wichiankoo <tharit_8@hotmail.com>
2019-01-24 20:58:40 +0700

    Update test.bat

--- server/jenkins/scripts/test.bat
+++ server/jenkins/scripts/test.bat
@@ -9,10 +9,7 @@ echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 cd %cd%/server
-set -x
-set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
-set -x
