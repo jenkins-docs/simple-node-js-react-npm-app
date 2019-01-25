b6201aa5f537191c8bc45776e826ea3ee29a6e11
Tharit Wichiankoo <tharit_8@hotmail.com>
2019-01-25 22:26:20 +0700

    test jenkins

--- client/jenkins/scripts/test.bat
+++ client/jenkins/scripts/test.bat
@@ -16,4 +16,4 @@ echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
-npm test
+
