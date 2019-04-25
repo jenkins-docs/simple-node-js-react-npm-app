74e02a55bffdaee6b945671dfb7e5468160e2ef6
alexislopes <alexisdrops@gmail.com>
2019-04-25 15:35:54 -0300

    added -x to  push.sh

--- jenkins/scripts/push.sh
+++ jenkins/scripts/push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 echo 'The following "git" command creates a new remote to the production repository'
-set +x
-git push
+set -x
+sudo git push
 echo 'the Git command ran successfully'
