d9eed9d4918f8d75f3c6b807e26b0fe1e5b3b1fd
alexislopes <alexisdrops@gmail.com>
2019-04-25 15:23:58 -0300

    Modified push.sh

--- jenkins/scripts/push.sh
+++ jenkins/scripts/push.sh
@@ -2,5 +2,5 @@
 
 echo 'The following "git" command creates a new remote to the production repository'
 set +x
-git remote add prod https://github.com/alexislopes/prod-test.git
+git push
 echo 'the Git command ran successfully'
