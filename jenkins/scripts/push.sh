922d22a71fbb69c532048c40febef1552bb1fd56
alexislopes <alexisdrops@gmail.com>
2019-04-26 11:25:33 -0300

    deleted push.sh

--- jenkins/scripts/push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-echo 'The following "git" command creates a new remote to the production repository'
-set -x
-git remote add prod https://github.com/alexislopes/prod-test.git
-
-
-git push prod master
-echo 'the Git command ran successfully'
