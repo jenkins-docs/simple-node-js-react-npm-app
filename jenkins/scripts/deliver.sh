f67e4368504310be01ae565baa5e8c95158013c0
alexislopes <alexisdrops@gmail.com>
2019-04-29 14:22:39 -0300

    fdsfsdfsdfsdfsdfsdfsdfsdf

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,8 +5,8 @@ echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
-git checkout development
 set -x
+git checkout development
 npm run build
 set +x
 
