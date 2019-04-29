1128daee9e38d54e77792e8d7e28f41a939ffc50
alexislopes <alexisdrops@gmail.com>
2019-04-29 11:00:58 -0300

    test2

--- jenkins/scripts/publish.sh
+++ jenkins/scripts/publish.sh
@@ -1,4 +1,4 @@
-echo 'Nesta etapa o Jenkins irá publicar o código na branch "prod" do repositório no Github'
+echo 'Nesta etapa o Jenkins irá publicar o código na branch "prod" do repositório no Github '
 
 set -x
 echo $"push from Jenkins" >> "./pushes.log"
