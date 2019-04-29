beb95fa45408d7ef048d6cd5d2e4b61b7bdebc74
alexislopes <alexisdrops@gmail.com>
2019-04-29 14:01:05 -0300

    fdsfsdfsdfsdfsdfsdfsdfsdf

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Node +  React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload oi td bem
+          To get started, edit <code>src/App.js</code> and save to reload oi td bem SONEMBLENEM
         </p>
       </div>
     );
