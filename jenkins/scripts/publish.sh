f0dbc69808267343cac630e14b36805061bb9f79
alexislopes <alexisdrops@gmail.com>
2019-04-29 10:58:07 -0300

    test

---
+++ jenkins/scripts/publish.sh
@@ -0,0 +1,7 @@
+echo 'Nesta etapa o Jenkins irá publicar o código na branch "prod" do repositório no Github'
+
+set -x
+echo $"push from Jenkins" >> "./pushes.log"
+git add .
+git commit -m "pushed from jenkins"
+git merge prod
