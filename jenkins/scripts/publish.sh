888d51f4299d3a6055fefed8cc6d0f09f462bc95
alexislopes <alexisdrops@gmail.com>
2019-04-29 14:12:38 -0300

    fdsfsdfsdfsdfsdfsdfsdfsdf

--- jenkins/scripts/publish.sh
+++ jenkins/scripts/publish.sh
@@ -1,6 +1,10 @@
 echo 'Nesta etapa o Jenkins irá publicar o código na branch "prod" do repositório no Github '
 
 set -x
+
+git config user.email alexisdrops@gmail.com
+git config user.name alexislopes
+
 git branch
 
 git merge origin/development
