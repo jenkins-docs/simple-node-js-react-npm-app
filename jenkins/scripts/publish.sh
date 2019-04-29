c497b765b25935da1706c36c9a3204c85e934401
alexislopes <alexisdrops@gmail.com>
2019-04-29 12:59:22 -0300

    oloia

--- jenkins/scripts/publish.sh
+++ jenkins/scripts/publish.sh
@@ -1,7 +1,6 @@
 echo 'Nesta etapa o Jenkins irá publicar o código na branch "prod" do repositório no Github '
 
 set -x
-echo $"push from Jenkins" >> "./pushes.log"
 git add .
 git commit -m "pushed from jenkins"
 git checkout prod
