db1c8a42b5944e9340df9173a824417a379cb52e
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-09-03 13:49:54 +0200

    updates

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders without crashing and having idiots', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML.includes('Idiots')).toBeTruthy()
+});
