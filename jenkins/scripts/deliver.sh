e435ea7687738ffd4546267cef92edaf034f1d73
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-09-03 19:31:56 +0200

    updates

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -9,8 +9,8 @@ set -x
 npm run build
 set +x
 
-set -x
-docker build . -t aabdelhay/test-app
-docker login --username=aabdelhay --email=ahmed.abdelhay@andela.com
-docker push aabdelhay/test-app
-set +x
+#set -x
+#docker build . -t aabdelhay/test-app
+#docker login --username=aabdelhay --email=ahmed.abdelhay@andela.com
+#docker push aabdelhay/test-app
+#set +x
