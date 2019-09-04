1f5adc7430b9f69dc75ed566bee4714134e26de2
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-09-04 14:51:38 +0200

    adding linting rules

--- src/App.test.js
+++ src/App.test.js
@@ -10,34 +10,5 @@ it('renders without crashing', () => {
 it('renders without crashing and having idiots', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  expect(div.innerHTML.includes('Idiots')).toBeTruthy()
+  expect(div.innerHTML.includes('Idiots')).toBeTruthy();
 });
-
-/**
- * pipeline {
-    agent {
-        docker {
-            image 'node:10-alpine'
-            args '-p 3000:3000'
-        }
-    }
-    stages {
-        stage('Install') {
-            steps {
-                sh 'npm install'
-            }
-        }
-        stage('Test') {
-            steps {
-                sh 'CI=true npm test'
-            }
-        }
-        stage('Build') {
-            steps {
-                sh 'npm run build'
-            }
-        }
-
-    }
-}
- **/
