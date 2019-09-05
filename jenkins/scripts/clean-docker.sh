61de5bd14fd9e7412069db337bf4b9a0783a642b
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-09-05 18:13:42 +0200

    adding file of cleanning docker up

--- jenkins/scripts/clean-docker.sh
+++ jenkins/scripts/clean-docker.sh
@@ -3,19 +3,19 @@
 #sh 'docker rmi -f $(docker images registry-1.docker.io/aabdelhay/test-app -q)'
 
 #removing container
-containers=`docker ps -q`
-if [ ! -z containers ]
+containers=`docker ps -aq`
+if [ ! -z $containers ]
 then
-docker stop $(docker ps -q)
-docker rm $(docker ps -q)
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 else
 echo "No containers found for cleanup"
 fi
 #removing images
 images=`docker images registry-1.docker.io/aabdelhay/test-app -q`
-if [ ! -z images ]
+if [ ! -z $images ]
 then
-docker rmi -f $(docker images registry-1.docker.io/aabdelhay/test-app -q)
+docker rmi -f $(docker images aabdelhay/test-app -q) | exit 0
 else
 echo "No images found for cleanup"
 fi
