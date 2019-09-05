483a7f62226176b7c83c63ce3c634d9316c8fd85
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-09-06 00:00:52 +0200

    addiing dummy test case

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('App component', function() {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
 
-it('renders without crashing and having idiots', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML.includes('Welcom')).toBeTruthy();
+  it('renders without crashing and having', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    expect(div.innerHTML.includes('Welcom')).toBeTruthy();
+  });
+
+  it('renders without crashing and not having idiots', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    expect(div.innerHTML.includes('idiots')).toBeFalsy();
+  });
 });
