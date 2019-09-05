a7d8522d2cddb7c13474f108997926ee9694931e
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-09-05 16:25:20 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React Idiots</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Welcome to React</h1>
       </header>
-      <p className="App-intro">
+      <p className='App-intro'>
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
