fdffaf441ac48a639a950c9ea116c45cdd4c8af1
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-09-12 12:10:56 +0200

    update to test ui

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <h1 className='App-title'>Welcome to React</h1>
       </header>
+      <div>Hello World!</div>
     </div>
   );
 }
