ef1e8eb146d5a3a4f6de6510938b129a6d00bae6
anuradhasg <44811898+anuradhasg@users.noreply.github.com>
2020-06-23 19:46:35 +0530

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -9,6 +9,7 @@ echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
+echo 'Test Commit from GIT'
 set -x
 # npm install --save-dev cross-env
 set +x
