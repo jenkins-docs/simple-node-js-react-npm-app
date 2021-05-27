3d308f4e3b4e609c586c3ec38594e7ca6d5904b2
Qi Wang <q.wang@efiport.de>
2021-05-27 12:06:09 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. I'm
-          production!
+          production!!
         </p>
       </div>
     );
