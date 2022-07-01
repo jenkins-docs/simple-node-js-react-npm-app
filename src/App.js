bffcd1544dc5240db6de791a8fd291e6a43aa15f
Michael Pearce <michael@yoga7i.localdomain>
2022-07-01 18:32:42 +1000

    Updated styling in App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to My React</h1>
         </header>
+        <p>A quick brown fox <span className='MySpan'>jumps</span> over the lazy dog.</p>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
