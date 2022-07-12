835286641c24ea6825c9e9cd8d9547d24d427492
Shivansh Rajpoot <shivanshrajpoot@sharechat.co>
2022-07-12 17:26:11 +0530

    Update : homescreen msg

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
+          <p>We are doing some changes here</p>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
