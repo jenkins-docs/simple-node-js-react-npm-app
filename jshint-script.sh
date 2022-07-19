d8ec49905814f7a5ec65ed4541a742e52bc94a7d
nitinpandey01 <106578178+nitinpandey01@users.noreply.github.com>
2022-07-19 11:53:09 +0530

    Update jshint-script.sh

--- jshint-script.sh
+++ jshint-script.sh
@@ -2,6 +2,6 @@
 
 cd /{JENKINS HOME DIRECTORY}/workspace/node-lint-pipeline
 
-jshint $(ls ./**/*.js ./**/*.ejs | grep -v node | grep js) *.js > /{JENKINS HOME DIRECTORY}/reports/jshint-report
+jshint $(ls ./**/*.js ./**/*.ejs | grep -v node | grep js) *.js > /var/lib/jenkins/reports/jshint-report
 
 echo $? > /dev/null
