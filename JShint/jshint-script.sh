2029ca61861ae5d77936d80fc1a876111a3f6a85
nitinpandey01 <106578178+nitinpandey01@users.noreply.github.com>
2022-07-19 12:08:59 +0530

    Delete JShint directory

--- JShint/jshint-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /{JENKINS HOME DIRECTORY}/workspace/node-lint-pipeline
-
-jshint $(ls ./**/*.js ./**/*.ejs | grep -v node | grep js) *.js > /var/lib/jenkins/reports/jshint-report
-
-echo $? > /dev/null
