8dc78b5e6ada22ae586713c032fc790ba11db079
nitinpandey01 <106578178+nitinpandey01@users.noreply.github.com>
2022-07-20 11:36:10 +0530

    Update .eslintrc.js

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,15 +1,1 @@
-module.exports = {
-    "root": true,
-    "parserOptions": {
-         "ecmaVersion": latest,
-         "sourceType": "module"
-    },
-    "extends": "eslint:recommended"
-    "env": { 
-         "es2021": true,
-         "node": true
-    },
-    "rules": {
-         "no-console": "error"
-    }
-}
+
