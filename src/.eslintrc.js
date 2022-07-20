2da6bde6dcd54aa49623c97662e53b8b4e442f04
nitinpandey01 <106578178+nitinpandey01@users.noreply.github.com>
2022-07-20 10:55:05 +0530

    Create .eslintrc.js

---
+++ src/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "root": true,
+    "parserOptions": {
+         "ecmaVersion": latest,
+         "sourceType": "module"
+    },
+    "extends": "eslint:recommended"
+    "env": { 
+         "es2021": true,
+         "node": true
+    },
+    "rules": {
+         "no-console": "error"
+    }
+}
