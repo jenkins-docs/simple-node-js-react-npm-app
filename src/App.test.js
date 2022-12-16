2cf99dd28bc57ec9e775e1d752bc5d9c46ef438f
Alexander Brandes <mc.cache@web.de>
2022-12-16 18:57:47 +0100

    Make this work with modern react

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDom.render(
+    <StrictMode>
+      (<App />, div)
+    </StrictMode>,
+    document.getElementById('root')
+  )
 });
