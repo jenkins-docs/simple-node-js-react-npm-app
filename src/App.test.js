2191ceda789e00366f1a5a4de8cbc020a3b62736
Alexander Brandes <mc.cache@web.de>
2023-03-14 22:15:19 +0100

    Migrate from 'ReactDom' to 'ReactDOM'

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,9 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDom.render(
+  ReactDOM.createRoot(div).render(
     <StrictMode>
-      (<App />, div)
-    </StrictMode>,
-    document.getElementById('root')
-  )
+      <App />
+    </StrictMode>
+  );
 });
