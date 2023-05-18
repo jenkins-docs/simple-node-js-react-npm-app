18c8b711979fede9f30e2f61dc2677f94660f547
phuocphat <37285767+phuocphat@users.noreply.github.com>
2023-05-18 18:24:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 alshkdkashjdio211
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App1 />, document.getElementById('root'));
 registerServiceWorker();
+var AWS = require('aws-sdk'); 
+
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = "AKIAIOSFODNN7EXAMPLE";
+AWS.config.secretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+AWS.config.region = "us-east-1";
