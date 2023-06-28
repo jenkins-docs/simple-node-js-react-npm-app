20022a9c42df3f2864d6fdab86d95d853a869cdc
Bruno Verachten <gounthar@gmail.com>
2023-06-28 17:52:57 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,7 +6,7 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
-npm run build
+npm_config_loglevel=verbose npm run build
 set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
@@ -18,7 +18,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+npm_config_loglevel=verbose npm start &
 sleep 1
 echo $! > .pidfile
 set +x
