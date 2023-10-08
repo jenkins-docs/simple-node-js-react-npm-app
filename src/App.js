d3413d226dd7f5984ffaec6c31a57bb921e58468
baronix <baronwork@icloud.com>
2023-10-08 01:41:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
