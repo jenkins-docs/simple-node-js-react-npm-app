102f2ea14f0a4a5bfe8bfe11505dd5a714737104
wifi_mug <yaxding@gmail.com>
2023-11-14 10:16:54 +0000

    Finished tutorial

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Finished tutorial yay. Setting up Git.
+        </p>
       </div>
     );
   }
