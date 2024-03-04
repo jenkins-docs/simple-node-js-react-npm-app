48c3c59a0e38fd6c98801877d7f79b1740dc4bca
haidao247 <haidao777@gmail.com>
2024-03-04 22:47:52 +1100

    add-sapce

--- jenkins/read.sh
+++ jenkins/read.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
-fp = ./jenkins/scripts/
+fp="./jenkins/scripts/"
 
 for file in "$fp"/*; do
 if [ -f "$file" ]; then
     echo "file: $file "
     echo "content"
     cat "$file"
+    fi
+
+    done
